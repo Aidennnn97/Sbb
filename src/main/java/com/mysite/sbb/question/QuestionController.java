e128324b28b4aaf40fb220c8cea96c084d1c37bc
Aidennnn97 <aidennnn97@gmail.com>
2023-03-27 16:29:22 +0900

    Url Prefix

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/question")
 public class QuestionController {
     private final QuestionService questionService;
 
@@ -18,14 +20,14 @@ public class QuestionController {
         return "redirect:/question/list";
     }
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model){    // Model 객체는 자바 클래스와 템플릿 간의 연결고리 역할
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
 
-    @GetMapping("/question/detail/{id}")
+    @GetMapping("/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id){
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
